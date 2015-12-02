9749fe56fe49f6e588a1f16183d8632604e18fe6
pubgin <aliaksandr.siarohin@gmail.com>
2015-12-02 23:28:09 +0100

    Remove comment about old file format.

--- tasks_file_parser.h
+++ tasks_file_parser.h
@@ -10,18 +10,6 @@
 namespace NRTSimulator {
 	class TTaskFileParser {
 	public:
-		/*
-		* Number_Of_Tasks
-		* Task_1_Name
-		* Task_1_CPU
-		* Task_1_Priority
-		* Task_1_Perion		
-		* Task_1_Number_Of_Execution_Time_masses
-		* Task_1_Mass_1 Task_1_Execution_Time_1
-		* ...
-		* Taks_2_Name
-		* ...
-		*/
 		std::vector<std::shared_ptr<TTask>> Parse(const std::string &);
 	};
 }
